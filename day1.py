a9c290167ae03c31c05715c157f4ae417b0db919
Maithreyi Gopal <maithreyi.gopal@gmail.com>
2021-09-25 22:25:27 +0200

    print statements with some string manipulation

---
+++ day1.py
@@ -0,0 +1,29 @@
+#day1: print statement | string manipulation |
+#remember indentation matters a lot :( 
+#python also acts as an intepretor and only gives you one error at a time :( 
+
+print ("I remember how to print text and use the print character\n") # the double quotes is used for text
+print ("---Practice----------------------------------------------")
+print ("Day 1 - Python Print Function")
+print ("The function is declarewd like this")
+print ("print('what to print')") #notice I've used single quote cause the double quote is outside
+print ("---End of practice---------------------------------------\n")
+
+#the next 2 statements work the same way
+print ("print(\"what to print\")")
+print ('print("what to print")')
+
+
+#string and string manipulation
+print ("Hello world\nHello world")
+
+#string concatination
+print ("hello " + "2nd part of the string\n")
+
+print ("---Practice----------------------------------------------")
+print ("Day 1 - String Manipulation")
+print ("String Concatination is done with the \"+\" sign")
+print ("e.g. print(\"Hello\" + \"world\")")
+print ("New  lines can be created with a backslash and n")
+print ("---End of practice---------------------------------------\n")
+
