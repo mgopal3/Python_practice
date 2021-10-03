ae9dc4759c395595f4fded0a6d90671dafd0f347
Maithreyi Gopal <maithreyi.gopal@gmail.com>
2021-10-03 11:37:40 +0200

    completed the day1 practice on udemy. Understnad printing and variables

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
-#day1: print statement | string manipulation |
+#day1: print statement | string manipulation | input data | variables
 #remember indentation matters a lot :( 
 #python also acts as an intepretor and only gives you one error at a time :( 
 
@@ -26,4 +26,56 @@ print ("String Concatination is done with the \"+\" sign")
 print ("e.g. print(\"Hello\" + \"world\")")
 print ("New  lines can be created with a backslash and n")
 print ("---End of practice---------------------------------------\n")
+
+#input functions 
+input("A prompt for the user")
+input("what is your name?")
+
+#here we are using the input and printint it. In this case first the input is evaluated and then the value is printed as hello <input>
+print("Hello " + input("what is your name?")) 
+
+print ("---Practice----------------------------------------------")
+#pick input from the user and get the number of characters in the string
+print(len(input ("What is your name? ")))
+print ("---End of practice---------------------------------------\n")
+
+#python variable
+name = input("what is your name? ")
+print (name)
+#variables can be reassigned with anything
+name = 10
+print (name)
+
+print ("---Practice----------------------------------------------")
+#pick 2 variables and exchange their values
+a = input("a = ");
+b = input("b = ");
+
+print("a = " + b)
+print("b = " + a)
+
+#or
+
+d = a
+a = b
+b = d
+
+print("a = " + a)
+print("b = " + b)
+
+print ("---End of practice---------------------------------------\n")
+
+#Variable naming
+#seperate words with _ and don't use number in the begining
+#don't use key words
+
+print ("---Final Practice----------------------------------------\n")
+#Band Name generator. Input city and pets name and concatinate the 2 for a band name
+print ("Welcome to the Band Name Generator\n")
+city = input ("What's the name of the city you grew up in?\n")
+pet = input ("What's the name of your pet?\n")
+band_name = city + " " + pet
+print("Your band name could be " + band_name)
+print ("---End of practice---------------------------------------\n")
+
 
