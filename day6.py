5db9569fac148c92a7927a125a67e1309293baa9
Maithreyi Gopal <maithreyi.gopal@gmail.com>
2021-11-07 21:52:46 +0100

    functions and while loop

---
+++ day6.py
@@ -0,0 +1,25 @@
+#Functions and while loops
+
+#we've used tons of predefined functions. Now we define our own
+
+#def funcName():
+	#do something
+
+def sample():
+	print("Hello")
+	print("use")
+
+#calling the function
+sample()
+
+#While Loop:
+#A loop that will continue while a particular condition is true
+#while somthing_is_true:
+	#do something
+
+#while something != True
+
+#or
+
+#while not something #means while something is false
+
